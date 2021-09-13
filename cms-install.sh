e352a89a71eebe9fe57dba447670dc3254a601b7
Izzxt <haziqizzat89@gmail.com>
2021-09-13 22:17:49 +0800

    Change to Composer v2

--- cms-install.sh
+++ cms-install.sh
@@ -245,8 +245,11 @@ cosmic_dep(){
 
   sudo apt-get -y install php-mysql
 
-  # Install Composer
-  sudo apt-get -y install composer
+  # Install Composer v2
+  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+  php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+  sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+  php -r "unlink('composer-setup.php');"
 
   # Install Git
   sudo apt-get -y install git
